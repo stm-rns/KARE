c49417142a365af3308706a00ad81cb802f95f3c
Benoit DUFLOS <benoit.duflos@st.com>
2023-07-28 15:20:29 +0200

    bmp565 generator update

--- 05-Tools/bmp565_128x128_generator.py
+++ 05-Tools/bmp565_128x128_generator.py
@@ -1,6 +1,7 @@
-import os,sys,struct,wx
+import os,sys,struct
 from PIL import Image
-
+from tkinter import filedialog
+ 
 def write_bin(f, pixel_list):
     for pix in pixel_list:
         r = (pix[0] >> 3) & 0x1F
@@ -8,29 +9,21 @@ def write_bin(f, pixel_list):
         b = (pix[2] >> 3) & 0x1F
         f.write(struct.pack('H', (r << 11) + (g << 5) + b))
 
-app = wx.App()
-dlg = wx.FileDialog(None,"Pick up file",'.',"","JPG|*.jpg|PNG|*.png|BMP|*.bmp|*.*|*.*",wx.FD_OPEN)
-if wx.ID_OK !=  dlg.ShowModal():
-    wx.MessageBox('canceled','canceled')
-    dlg.Destroy()
-    sys.exit()
+path = filedialog.askopenfilename(filetypes=[("jpeg","*.jp*g"),("png","*.png"),("bmp","*.bmp"),("all files", "*")])
 
-path = dlg.GetPath()
-dlg.Destroy()
+if not path:
+    sys.exit(1)
 
 im = Image.open(path)
 im = im.resize((128,128))
 im = im.convert('RGB')
 pixels = list(im.getdata())
 
-dlg = wx.FileDialog(None,"Pick up file",'.',"output.bin","BIN|*.bin|PNG|*.png|BMP|*.bmp|*.*|*.*",wx.FD_SAVE)
-if wx.ID_OK !=  dlg.ShowModal():
-    wx.MessageBox('canceled','canceled')
-    dlg.Destroy()
-    sys.exit()
+initialfile = os.path.splitext(path.split(os.sep)[-1])[0]+'.bin'
+out_path = filedialog.asksaveasfilename(filetypes=[("bin","*.bin")],initialfile=initialfile)
 
-out_path = dlg.GetPath()
-dlg.Destroy()
+if not out_path:
+    sys.exit(2)
 
 with open(out_path, 'wb') as f:
     write_bin(f, pixels)
